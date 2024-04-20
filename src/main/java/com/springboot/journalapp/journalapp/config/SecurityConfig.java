f2e7908d49495d0be2c1494226b622e1c03f4442
MIBGHOST <heisenbergnr@gmail.com>
2024-04-21 00:45:44 +0530

    jakarta.servlet-api added & securityConfig dependencies were removed

--- src/main/java/com/springboot/journalapp/journalapp/config/SecurityConfig.java
+++ src/main/java/com/springboot/journalapp/journalapp/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import com.springboot.journalapp.journalapp.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,15 +22,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/journal/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .httpBasic();
-
-        return http.build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/journal/**","/users/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
